1f89a558362cb1a147f86162bc366a0b382777de
4019 <fas_saf28@yahoo.com>
2020-10-02 11:58:15 +0930

    Removed .gitattributes

--- Matplotlib_venv/PyPlots/BarChartFromCSV.py
+++ Matplotlib_venv/PyPlots/BarChartFromCSV.py
@@ -1,3 +1,37 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9c77383417400fee5b9dbd6816c8fd55a48759faf7a06c61b0722ff55fc3bea7
-size 805
+import csv
+import numpy as np
+from collections import Counter
+from matplotlib import pyplot as plt
+import pandas as pd
+
+#plt.style.use("fivethirtyeight")
+
+data = pd.read_csv('../DataFiles/data.csv')
+ids = data['Responder_id']
+lang_responses = data['LanguagesWorkedWith']
+
+language_counter = Counter()
+
+for response in lang_responses:
+    language_counter.update(response.split(';'))
+
+languages = []
+popularity = []
+
+#print(language_counter.most_common(15))
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+
+# print(languages)
+# print(popularity)
+languages.reverse()
+popularity.reverse()
+# print(languages)
+# print(popularity)
+
+plt.barh(languages, popularity)
+plt.xlabel('Number of People Who Use')
+plt.title('Most Popular Languages')
+plt.show()
